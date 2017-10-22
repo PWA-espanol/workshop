8cd704d94f4ff124c329fd4098f0fca1199bcc01
Nicolas Bello Camilletti <nbellocamilletti@gmail.com>
2017-10-21 21:52:08 -0300

    Add begin solution to module 2

---
+++ 02-proyecto/code/public/js/common.js
@@ -0,0 +1,81 @@
+const serverUrl = 'http://localhost:3000/';
+function apiClient(url, options, success, error) {
+    options = options || {};
+    let request = new XMLHttpRequest();
+    
+    request.open(options.method || 'get', url);
+    request.setRequestHeader('Content-type', 'application/json');
+
+    request.onload = () => {
+        if (request.status == 200 && request.getResponseHeader('Content-Type').indexOf('application/json') !== -1) {
+            const responseObj = JSON.parse(request.response);
+            if (success) {
+                success(responseObj);
+            }
+        } else {
+            throw new TypeError();
+        }
+    };
+
+    request.onerror = error;
+
+    request.send(options.body);
+}
+
+function saveExpense(expense, cb) {
+    apiClient(`${serverUrl}api/expense/${expense.id || ''}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(expense)
+    }, cb);
+}
+
+function deleteExpenses(cb) {
+    try {
+        apiClient(`${serverUrl}api/expense`, { method: 'DELETE' }, cb);
+    } catch (error) {
+        alert("Error deleting expenses");
+    }
+}
+
+function getExpenses(cb) {
+    apiClient(`${serverUrl}api/expense`, {}, cb)
+}
+
+function getExpense(expenseId, cb) {
+    apiClient(`${serverUrl}api/expense/${expenseId}`, {}, cb);
+}
+
+function createNewExpense() {
+    return {
+        name: 'Nombre',
+        details: []
+    };
+}
+
+function getExpenseTotal(expense) {
+    let total = 0;
+    if (expense && expense.details) {
+        expense.details.forEach( item => {
+            total += item.cost;
+        });
+    }
+
+    return total;
+}
+
+function getTotal(expenses) {
+    let total = 0;
+    expenses.forEach( expense => {
+        total += getExpenseTotal(expense);
+    });
+
+    return total;
+}
+
+function share(title) {
+    const url = window.location.href;
+    window.open("http://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url), '_blank');
+}
