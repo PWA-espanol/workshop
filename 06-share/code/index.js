eab2e34c63419360630f7ef4ebe174ce939acfc1
Nicolas Bello Camilletti <nbellocamilletti@gmail.com>
2017-10-23 12:46:57 -0300

    Add module 6 end solution

---
+++ 06-share/code/index.js
@@ -0,0 +1,65 @@
+const Repository = require('./repository');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.get('/api/expense', function (req, res) {
+  console.log(`---- GET ('/api/expense/')`);
+
+  res.json(Repository.getExpenses());
+});
+
+app.get('/api/expense/:id', function (req, res) {
+  console.log(`---- GET ('/api/expense/${req.params.id}')`);
+
+  const id = parseInt(req.params.id, 10);
+  res.json(Repository.getExpense(id));
+});
+
+app.delete('/api/expense', function (req, res) {
+    console.log(`---- DELETE ('/api/expense')`);
+    Repository.deleteExpenses();
+    return res.json(Repository.getExpenses());
+});
+
+app.post('/api/expense/:id', function (req, res) {
+  console.log(`---- POST ('/api/expense/${req.params.id}')
+---- ---- Body: ${JSON.stringify(req.body)}`);
+
+  const expense = req.body;
+  if (!expense.id) {
+    expense.id = req.params.id;
+  }
+
+  Repository.saveExpense(expense);
+  return res.json(Repository.getExpenses());
+});
+
+app.post('/api/expense/', function (req, res) {
+  console.log(`---- POST ('/api/expense/')
+----Body: ${JSON.stringify(req.body)}`);
+
+  Repository.saveExpense(req.body);
+  return res.json(Repository.getExpenses());
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/expense/:id', function (req, res) {
+  if (!req.params.id) {
+    res.redirect('/');
+  }
+
+  res.sendFile(__dirname + '/public/expense.html');
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
---
+++ 06-share/code/public/service-worker.js
@@ -0,0 +1,70 @@
+(function() {
+    'use strict';
+    
+    const CACHE_NAME = 'static-cache';
+    const urlsToCache = [
+        '.',
+        'index.html',
+        'css/bootstrap.css',
+        'css/custom.css',
+        'js/common.js',
+        'js/expenses.js',
+        'js/home.js'
+    ];
+
+    self.addEventListener('install', (event) => {
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(function(cache) {
+                    return cache.addAll(urlsToCache);
+                })
+        );
+    });
+
+    self.addEventListener('activate', event => {
+        var keepList = [CACHE_NAME];
+    
+        event.waitUntil(
+            caches.keys().then(cacheNameList => {
+                return Promise.all(cacheNameList.map(cacheName => { 
+                    if (keepList.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                }));
+            })
+        );
+    });
+
+    self.addEventListener('fetch', event => {
+        console.log(event.request.url);
+
+        if (event.request.url.indexOf('/api/') !== -1) {
+            event.respondWith(fetchAndCache(event.request));
+        } else {
+            event.respondWith(
+                caches.match(event.request).then(response => {
+                    return response || fetchAndCache(event.request);
+                })
+            );
+        }
+    });
+    
+    function fetchAndCache(url) {
+        return fetch(url)
+            .then((response) => {
+                // Check if we received a valid response
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        cache.put(url, response.clone());
+                        return response;
+                    });
+            })
+            .catch((error) => {
+                console.log('Request failed:', error);
+                // You could return a custom offline 404 page here
+            });
+    }
+})();
